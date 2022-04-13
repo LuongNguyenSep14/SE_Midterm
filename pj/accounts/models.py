87303b51a024818d8d5956c1849e7e76e8a3d155
luong12320 <nguyenminhluong14092001@gmail.com>
2022-04-13 23:57:24 +0700

    + Register: {products, customer, order} + Create super accounts + add frist customer

--- pj/accounts/models.py
+++ pj/accounts/models.py
@@ -1,3 +1,41 @@
+from pyexpat import model
+from sre_constants import CATEGORY
+from statistics import mode
+from unicodedata import category
 from django.db import models
+from sqlalchemy import null
+from tables import Description
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Deliveried', 'Deliveried'),
+    )
+
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
